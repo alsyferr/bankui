8bb24a83abef91dcaa31112c0abb5b34e350e03c
Richy Sel <alvincypher@gmail.com>
2024-05-06 15:38:40 +0000

    implement auth , appwrite sentry

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import React from 'react'
 import { cn } from '@/lib/utils'
 import { usePathname } from 'next/navigation'
+import Footer from './Footer'
 
 const Sidebar = ({user}: SiderbarProps) => {
     const pathname = usePathname();
@@ -41,7 +42,7 @@ const Sidebar = ({user}: SiderbarProps) => {
         USER
      </nav>
 
-     FOOTER
+     <Footer user={user} />
     </section>
   )
 }
